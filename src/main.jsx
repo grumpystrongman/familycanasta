bea482514660deecc78b95a38410178c336dd52c
Jeff Barnes <38039967+grumpystrongman@users.noreply.github.com>
2026-07-11 09:09:36 -0400

    Mount animated game-over celebration

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import GameStateEnhancer from "./GameStateEnhancer";
+import GameCelebration from "./GameCelebration";
 import "./styles.css";
 import "./team.css";
 import "./teamStyles.css";
@@ -9,10 +10,12 @@ import "./play.css";
 import "./scoring.css";
 import "./flexibleGame.css";
 import "./stateEnhancer.css";
+import "./gameCelebration.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
     <GameStateEnhancer />
+    <GameCelebration />
   </React.StrictMode>
 );
