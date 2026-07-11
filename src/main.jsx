ca716e05ecde4384ed361171d57bd264c5ab77e2
Jeff Barnes <38039967+grumpystrongman@users.noreply.github.com>
2026-07-11 11:32:40 -0400

    Mount grouped meld enhancer

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import GameStateEnhancer from "./GameStateEnhancer";
 import GameCelebration from "./GameCelebration";
+import MultiMeldEnhancer from "./MultiMeldEnhancer";
 import "./styles.css";
 import "./team.css";
 import "./teamStyles.css";
@@ -14,11 +15,13 @@ import "./gameCelebration.css";
 import "./wildTarget.css";
 import "./cardAccessibility.css";
 import "./boardAccessibility.css";
+import "./multiMeld.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
     <GameStateEnhancer />
     <GameCelebration />
+    <MultiMeldEnhancer />
   </React.StrictMode>
 );
