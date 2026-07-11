1117eec9a525d5b0143d397a4bcdd64baec8291e
Jeff Barnes <38039967+grumpystrongman@users.noreply.github.com>
2026-07-11 07:28:17 -0400

    Load enhanced card and gameplay styles

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from "./App";
 import "./styles.css";
 import "./team.css";
 import "./teamStyles.css";
+import "./play.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode><App /></React.StrictMode>
