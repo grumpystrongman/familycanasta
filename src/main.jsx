37738aa22cfd2105f72796a70c9a437ff99b0732
Jeff Barnes <38039967+grumpystrongman@users.noreply.github.com>
2026-07-11 08:58:40 -0400

    Mount discard and round state enhancer

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import GameStateEnhancer from "./GameStateEnhancer";
 import "./styles.css";
 import "./team.css";
 import "./teamStyles.css";
 import "./play.css";
 import "./scoring.css";
 import "./flexibleGame.css";
+import "./stateEnhancer.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode><App /></React.StrictMode>
+  <React.StrictMode>
+    <App />
+    <GameStateEnhancer />
+  </React.StrictMode>
 );
