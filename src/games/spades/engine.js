224bce3a441b7e8f07c1979acde69814a5677206
Jeff Barnes <38039967+grumpystrongman@users.noreply.github.com>
2026-08-02 17:52:15 -0400

    use pure deck utilities in spades engine

--- src/games/spades/engine.js
+++ src/games/spades/engine.js
@@ -1,4 +1,4 @@
-import { createStandardDeck, shuffleCards, sortStandardHand } from "../../platform/StandardCard";
+import { createStandardDeck, shuffleCards, sortStandardHand } from "../../platform/standardDeck.js";
 
 export const SPADES_RULES = Object.freeze({
   players: 4,
