e5b71f81332f8932d78cc34aa4f58a186895ee87
Jeff Barnes <38039967+grumpystrongman@users.noreply.github.com>
2026-08-10 16:15:21 -0400

    Use embedded Punchline video sting

--- src/games/punchline/index.jsx
+++ src/games/punchline/index.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import "../../partyStage.css";
 import PartyStageGame from "../../platform/party/PartyStageGame";
+import { PARTY_INTRO_VIDEOS } from "../../platform/party/introVideos";
 import { punchlineDefinition } from "./model";
 
+const definition = { ...punchlineDefinition, introVideo: PARTY_INTRO_VIDEOS.punchline };
+
 export default function PunchlineGame() {
-  return <PartyStageGame definition={punchlineDefinition} />;
+  return <PartyStageGame definition={definition} />;
 }
