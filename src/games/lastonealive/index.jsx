310e4821d61ddcf8cbd3b4c003c9991d4322b66f
Jeff Barnes <38039967+grumpystrongman@users.noreply.github.com>
2026-08-10 16:15:27 -0400

    Use embedded Last One Alive video sting

--- src/games/lastonealive/index.jsx
+++ src/games/lastonealive/index.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import "../../partyStage.css";
 import PartyStageGame from "../../platform/party/PartyStageGame";
+import { PARTY_INTRO_VIDEOS } from "../../platform/party/introVideos";
 import { LAST_ONE_ALIVE_TRIVIA, lastOneAliveDefinition } from "./model";
 
-const definition = { ...lastOneAliveDefinition, trivia: LAST_ONE_ALIVE_TRIVIA };
+const definition = { ...lastOneAliveDefinition, trivia: LAST_ONE_ALIVE_TRIVIA, introVideo: PARTY_INTRO_VIDEOS.lastonealive };
 
 export default function LastOneAliveGame() {
   return <PartyStageGame definition={definition} />;
