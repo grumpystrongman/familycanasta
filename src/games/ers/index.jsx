e2bf728b8cd977e8bd2e98bd474c9751fb034f1c
Jeff Barnes <38039967+grumpystrongman@users.noreply.github.com>
2026-08-10 09:21:12 -0400

    Register Egyptian Rat Screw module

---
+++ src/games/ers/index.jsx
@@ -0,0 +1,5 @@
+import "../../platform/standardCards.css";
+import "./styles.css";
+export { default } from "./ERSGame";
+export { ERS_RULES } from "./engine";
+export const gameInfo = Object.freeze({ id: "ers", name: "Egyptian Rat Screw", players: "2–6" });
