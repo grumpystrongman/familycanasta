84d668f7e4800e44029667daf9ed69f5d80750db
Jeff Barnes <38039967+grumpystrongman@users.noreply.github.com>
2026-08-10 09:17:00 -0400

    Register Spoons game module

---
+++ src/games/spoons/index.jsx
@@ -0,0 +1,5 @@
+import "../../platform/standardCards.css";
+import "./styles.css";
+export { default } from "./SpoonsGame";
+export { SPOONS_RULES } from "./engine";
+export const gameInfo = Object.freeze({ id: "spoons", name: "Spoons", players: "3–6" });
