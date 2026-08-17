1f07b321cb49f1a7ffb1d0cff859a0d3f9a4d39d
Jeff Barnes <38039967+grumpystrongman@users.noreply.github.com>
2026-08-17 16:04:40 -0400

    Add Blackglass asset integrity manifest

---
+++ public/blackglass/asset-integrity.js
@@ -0,0 +1,6 @@
+export const BLACKGLASS_ASSET_EXPECTATIONS = Object.freeze({
+  "canonical-cast-atlas.jpg": { minBytes: 40000 },
+  "room-atlas.jpg": { minBytes: 90000 },
+  "weapon-atlas.jpg": { minBytes: 15000 },
+  "ruby-victim-base.jpg": { minBytes: 30000 },
+});
